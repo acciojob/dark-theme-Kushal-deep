c761af2b520b9190bdb1bdcfc0038135d4f2ea38
Kushal-deep <kushaldeep448@gmail.com>
2025-03-14 20:15:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+document.getElementById("swap").addEventListener("click",function(){
+	let main=document.getElementById("app");
+	if (main.classList.contains("day")) {
+    main.classList.remove("day");
+    main.classList.add("night");
+  } else {
+    main.classList.remove("night");
+    main.classList.add("day");
+  }
+})
 
