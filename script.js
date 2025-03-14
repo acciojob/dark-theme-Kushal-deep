78ff72c180c6adbe00115198829503ae2877374a
Kushal-deep <kushaldeep448@gmail.com>
2025-03-14 20:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-//your code here
+// //your code here
+// document.getElementById("swap").addEventListener("click",function(){
+// 	let main=document.getElementById("app");
+// 	if (main.classList.contains("day")) {
+//     main.classList.remove("day");
+//     main.classList.add("night");
+//   } else {
+//     main.classList.remove("night");
+//     main.classList.add("day");
+//   }
+// })
 document.getElementById("swap").addEventListener("click",function(){
-	let main=document.getElementById("app");
-	if (main.classList.contains("day")) {
-    main.classList.remove("day");
-    main.classList.add("night");
-  } else {
-    main.classList.remove("night");
-    main.classList.add("day");
-  }
+    let main = document.getElementById("app");
+    let button = document.getElementById("swap");
+
+    if (main.classList.contains("day")) {
+        main.classList.remove("day");
+        main.classList.add("night");
+
+        button.classList.remove("button_day");
+        button.classList.add("button_night");
+    } else {
+        main.classList.remove("night");
+        main.classList.add("day");
+
+        button.classList.remove("button_night");
+        button.classList.add("button_day");
+    }
 })
+
 
